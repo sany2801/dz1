0e7039c573b88d06bef45c70e4f71a28a8e8fb53
sany2801 <37817378+sany2801@users.noreply.github.com>
2021-05-19 22:18:38 +0300

    Add files via upload

---
+++ Новая папка (3)/script.js
@@ -0,0 +1,69 @@
+// let str = 'hello';
+// str.slice(0, 2);
+
+// let obj = new Object();
+// let object = {
+
+// };
+
+// const obj = {
+//     color: 'red',
+//     width: 20,
+//     'current height': 30
+// };
+
+// obj.color //red
+// obj['current height'] //30
+// delete obj.width
+
+// const obj = 's'
+// const obj = { [size]: '1'};
+// const obj = {
+//     color: 'red',
+//     wi
+// }
+// console.log()
+// console.log('lol' in ob);
+
+// let object = {
+//     color: 'red',
+//     width: 20,
+//     height: 30
+// };
+// for (let key in object) {
+//     console.log(key);
+    // console.log(object[key]);
+// }
+
+// let user = {
+//     name: 'Andrew',
+//     age: 20,
+//     email: 'emaile@xmpl'
+// };
+// for (let key in user) {
+//     console.log((key) +': '+ (user[key]));
+// }
+
+let user = {
+    name : 'john'
+}
+let admin = user;
+admin.name = 'Andrew';
+console.log(user.name)
+
+let cat = {
+    name: 'Vwefds',
+    getVoice: function(){
+        console.log('mmymymym mymymy')
+    }
+}
+cat.getVoice()
+
+
+// let cat = {
+//     name = 'FVSVSD'
+// getName:() {
+//     console.log(`My name is....' ${this.name}`);
+// }
+// }
+// cat.getVoice()
