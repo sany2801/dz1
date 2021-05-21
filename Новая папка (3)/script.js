539ef0e254ef0e96dae649c55e6cdf303d46cf3a
sany2801 <37817378+sany2801@users.noreply.github.com>
2021-05-21 22:16:32 +0300

    Update script.js

--- Новая папка (3)/script.js
+++ Новая папка (3)/script.js
@@ -33,6 +33,39 @@
 // for (let key in object) {
 //     console.log(key);
     // console.log(object[key]);
+
+// const student = {
+//   firstname: 'Polina',
+//   lastname: 'Ivanova',
+//   group: 'FE-57',
+// };
+// const { firstname: last, lastname, group } = student;
+// console.log(last) // Polina 
+// console.log(lastname) // Ivanova 
+// console.log(group) //FE-57
+
+
+// let array = new Array();
+// let art = [1, 20, 35, 48, { name: 1, age: 2 }, 'str', true];
+// console.log(art[4, ]);
+// let arr = Array.of(2);
+
+// const numers = [1, 2, 33];
+// for (let i = 0; i < numers.length; i++) {
+//   console.log(numers[i]);
+// }
+// for (let number of numers) {
+//   console.log(number);
+// }
+const stay = [1, 2, 5, 6];
+stay.forEach(function(number) {
+  console.log(number);
+});
+const stay1 = [5, 2, 5, 6];
+let result = stay1.map(function(number) {
+  console.log(number ** 2 + '!');
+  return (result)
+})
 // }
 
 // let user = {
